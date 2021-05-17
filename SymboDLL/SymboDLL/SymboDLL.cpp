4bb05f12ee6749f181305231680ebf807049b04d
georgt99 <33638816+georgt99@users.noreply.github.com>
2021-05-18 01:41:44 +0200

    clean-up of test stuff

--- SymboDLL/SymboDLL/SymboDLL.cpp
+++ SymboDLL/SymboDLL/SymboDLL.cpp
@@ -20,30 +20,6 @@ using namespace autodiff;
 static unsigned int myPlaceholderVariable;
 
 
-// JUST FOR TESTING - DELETE ME ASAP
-int add1(const int x)
-{
-    return x + 1;
-}
-
-// JUST FOR TESTING
-float magnitude(float x, float y, float z) { //Vector3dual for me i think...?
-    Vector3f vec(x, y, z);
-    return vec.norm();
-}
-void negate_vec2(float* vec_array) {
-	//Vector2f v(vec_array[0], vec_array[1]);
-	//Vector2f negated = v.inverse();
-	vec_array[0] = - vec_array[0]; vec_array[1] = - vec_array[1]; //vec_array[0] = negated.x(); vec_array[1] = negated.y();
-	return; // TODO: is this needed?
-}
-void add_vec2s(float* vec1_array, float* vec2_array, float* result_array){
-	Vector2f vec1(vec1_array[0], vec1_array[1]);
-	Vector2f vec2(vec2_array[0], vec2_array[1]);
-	Vector2f result = vec1 + vec2;
-	result_array[0] = result.x(); result_array[1] = result.y();
-}
-
 
 void symbolic_kinematic(
 	float* i_array, float* j_array,
--- SymboDLL/SymboDLL/SymboDLL.h
+++ SymboDLL/SymboDLL/SymboDLL.h
@@ -7,12 +7,6 @@
 #define SYMBOLINKAGE_API __declspec(dllimport)
 #endif
 
-// JUST FOR TESTING
-extern "C" SYMBOLINKAGE_API int add1(const int x);
-extern "C" SYMBOLINKAGE_API float magnitude(float x, float y, float z);
-extern "C" SYMBOLINKAGE_API void negate_vec2(float* vec);
-extern "C" SYMBOLINKAGE_API void add_vec2s(float* vec1_array, float* vec2_array, float* result_array);
-
 
 extern "C" SYMBOLINKAGE_API void symbolic_kinematic(
 	float* i_array, float* j_array,
